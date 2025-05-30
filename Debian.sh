7ac57ad341a3b9c41280b136589c01561150fbd4
Alexandre Garcia dos Santos <135478609+codedbyxande@users.noreply.github.com>
2025-05-30 16:12:41 -0300

    Update Debian.sh

--- Debian.sh
+++ Debian.sh
@@ -15,10 +15,15 @@ sudo systemctl enable gdm3
 # ====================================
 # 3. Instalar VS Code
 # ====================================
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
 sudo apt update
-sudo apt install -y code
+sudo apt install code # or code-insiders
+
 
 # ====================================
 # 4. Instalar Pop Shell (Tiling)
