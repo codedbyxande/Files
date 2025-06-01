6700ebf68c419981f2d8117511dcb67014ba1367
Alexandre Garcia dos Santos <135478609+codedbyxande@users.noreply.github.com>
2025-06-01 15:42:06 -0300

    Update and rename archGnome.sh to arch.sh

--- arch.sh
+++ arch.sh
@@ -8,28 +8,12 @@ sudo pacman -Syu --noconfirm
 # ====================================
 # 2. Instalar Componentes Base (GNOME Mínimo)
 # ====================================
-sudo pacman -S --noconfirm gnome-shell gnome-control-center gdm kitty nautilus flatpak
+sudo pacman -S --noconfirm plasma-desktop dolphin dolphin-plugins ffmpegthumbs ark kitty  flatpak kate
 sudo pacman -S --noconfirm hyprland rofi-wayland nwg-lock nwg-displays pop-icon-theme
-sudo systemctl enable gdm
+sudo systemctl enable sddm
 
 # ====================================
-# 3. Instalar VS Code 
-# ====================================
-paru -S --noconfirm visual-studio-code-bin
-
-# ====================================
-# 4. Instalar Pop Shell (Tiling)
-# ====================================
-sudo pacman -S --noconfirm nodejs npm typescript make git
-TEMP_DIR=$(mktemp -d -t pop-shell-XXXXXX)
-git clone https://github.com/pop-os/shell.git "${TEMP_DIR}/pop-shell"
-cd "${TEMP_DIR}/pop-shell"
-make local-install
-cd - > /dev/null
-rm -rf "${TEMP_DIR}"
-
-# ====================================
-# 5. Configurar Flatpak
+# 4. Configurar Flatpak
 # ====================================
 sudo pacman -S --noconfirm flatpak # Garante que flatpak esteja instalado
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
